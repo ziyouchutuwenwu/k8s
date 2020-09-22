0ac159be3023032923d15c5a027cc155e41b8ce1
ziyouchutuwenwu <ziyouchutuwenwu@gmail.com>
2020-09-22 11:21:40 +0800

    更新动态卷的例子

--- 基础学习/dynamic_volume/delete.sh
+++
@@ -1,8 +0,0 @@
-#! /usr/bin/env bash
-
-kubectl delete -f ./nfs-storage-class.yaml
-kubectl delete -f ./nfs-provisioner-deployment.yaml
-kubectl delete -f ./nfs-client-sa.yaml
-
-kubectl delete pv --all
-kubectl delete pvc --all
--- 基础学习/dynamic_volume/run.sh
+++
@@ -1,5 +0,0 @@
-#! /usr/bin/env bash
-
-kubectl create -f ./nfs-client-sa.yaml
-kubectl create -f ./nfs-provisioner-deployment.yaml
-kubectl create -f ./nfs-storage-class.yaml
